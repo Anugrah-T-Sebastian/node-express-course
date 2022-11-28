7243a2adfe081fce34678aac1bddd9cd4a1c5dc5
Anugrah Sebastian <87081811+Anugrah-T-Sebastian@users.noreply.github.com>
2022-11-28 21:32:46 +0530

    Task manager code updated: Minor updates

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,20 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const tasks = require('./routes/tasks');
-const connectDB = require('./db/connect');
-require('dotenv').config()                          //TO KEEP USERNAME AND PASSOWRD SECRET!
-const notFound = require('./middleware/not-found');
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config(); //TO KEEP USERNAME AND PASSOWRD SECRET!
+const notFound = require("./middleware/not-found");
 
 // MIDDLEWARE
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 app.use(express.json());
 
 // ROUTES
-app.get('/hello', (req, res) => {
-    res.send('Task manager app');
-})
+app.get("/hello", (req, res) => {
+  res.send("Task manager app");
+});
 
-app.use('/api/v1/tasks', tasks);
+app.use("/api/v1/tasks", tasks);
 
 app.use(notFound);
 
@@ -24,18 +24,17 @@ app.use(notFound);
 //app.patch('/api/v1/tasks/:id')    - update task
 //app.delete('/api/v1/tasks/:id')   - delete task
 
-const port = process.env.PORT || 3000;          // To set port according to availability of the server
+const port = process.env.PORT || 3000; // To set port according to availability of the server
 
 const start = async () => {
-    try{
-        // connectDB returns a promise
-        await connectDB(process.env.MONGO_URI);                                              //TO KEEP USERNAME AND PASSOWRD SECRET!
-        // IMPORTANT: ONLY START THE SERVER AFTER CONNECTION WITH DB IS ESTABLISHED
-        app.listen(port, console.log(`server is listening on port ${port}...`));
-    }
-    catch(error){
-        console.log(error);
-    }
-}
+  try {
+    // connectDB returns a promise
+    await connectDB(process.env.MONGO_URI); //TO KEEP USERNAME AND PASSOWRD SECRET!
+    // IMPORTANT: ONLY START THE SERVER AFTER CONNECTION WITH DB IS ESTABLISHED
+    app.listen(port, console.log(`server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 start();
